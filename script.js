9d7f277c1b6f03439feda6dab8f69a2f500d00d3
afifurrohman-id <afifurrohman.id@gmail.com>
2023-05-09 16:56:58 +0700

    update error page

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 function handleToggleMenu() {
-  const toggle = document.querySelector("nav .toggle");
-  const menu = document.querySelector("nav menu");
-  const linkMenu = document.querySelectorAll("nav menu a");
+  const toggle = document.querySelector('nav .toggle')
+  const menu = document.querySelector('nav menu')
+  const linkMenu = document.querySelectorAll('nav menu a')
 
-  toggle.addEventListener("click", () => {
-    toggle.classList.toggle("change");
-    menu.classList.toggle("slide");
-  });
-  linkMenu.forEach(link => {
-    link.addEventListener("click", () => {
-      menu.classList.remove("slide");
-      toggle.classList.remove("change");
-    });
-  });
+  toggle.addEventListener('click', () => {
+    toggle.classList.toggle('change')
+    menu.classList.toggle('slide')
+  })
+  linkMenu.forEach((link) => {
+    link.addEventListener('click', () => {
+      menu.classList.remove('slide')
+      toggle.classList.remove('change')
+    })
+  })
 }
 
-window.addEventListener('scroll', () => {
-  const buttonToTop = document.querySelector(".go-top");
-  if (window.scrollY >= 50) {
-    buttonToTop.style.display = "flex";
-    buttonToTop.addEventListener("click", () => {
-      window.scrollTo({ top: 0 });
-    });
-  } else buttonToTop.style.display = "none";
-})
+function setVisibilityButtonBackTop() {
+  window.addEventListener('scroll', () => {
+    const buttonToTop = document.querySelector('.go-top')
+    if (window.scrollY >= 50) {
+      buttonToTop.style.display = 'flex'
+      buttonToTop.addEventListener('click', () => {
+        window.scrollTo({ top: 0 })
+      })
+    } else buttonToTop.style.display = 'none'
+  })
+}
 
-document.querySelector("nav") && handleToggleMenu();
+setVisibilityButtonBackTop()
+document.querySelector('nav') && handleToggleMenu()
